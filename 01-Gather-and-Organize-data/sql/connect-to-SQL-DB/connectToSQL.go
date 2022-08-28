34ed0059a844fa476df26f432afa2119faf58160
petrostrak <pit.trak@gmail.com>
2022-08-29 00:13:23 +0300

    Querying the database

--- 01-Gather-and-Organize-data/sql/connect-to-SQL-DB/connectToSQL.go
+++ 01-Gather-and-Organize-data/sql/connect-to-SQL-DB/connectToSQL.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"database/sql"
-	"log"
+	"fmt"
 	"os"
 
 	// pq is the libary that allows us to connect
@@ -16,14 +16,16 @@ func main() {
 	// an environmental variable.
 	pgURL := os.Getenv("PGURL")
 	if pgURL == "" {
-		log.Fatal("PGURL empty")
+		fmt.Println("PGURL empty")
+		os.Exit(1)
 	}
 
 	// Open a database value.  Specify the postgres driver
 	// for databases/sql.
 	db, err := sql.Open("postgres", pgURL)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 	defer db.Close()
 
@@ -32,6 +34,53 @@ func main() {
 	// for later use.  To make sure the database is available and
 	// accessible, we will use db.Ping().
 	if err := db.Ping(); err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Query the DB
+	rows, err := db.Query(`
+		SELECT
+			sepal_length as sLength,
+			sepal_width as sWidth,
+			petal_length as pLength,
+			petal_width as pWidth
+		FROM iris
+		WHERE species = $1`, "Iris-setosa")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer rows.Close()
+
+	// Iterate over the rows, sending the results to
+	// standard out.
+	for rows.Next() {
+		var (
+			sLength float64
+			sWidth  float64
+			pLength float64
+			pWidth  float64
+		)
+
+		err := rows.Scan(
+			&sLength,
+			&sWidth,
+			&pLength,
+			&pWidth,
+		)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%.2f, %.2f, %.2f, %.2f\n", sLength, sWidth, pLength, pWidth)
+
+		// Check for errors after we are done iterating over rows.
+		err = rows.Err()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	}
 }
