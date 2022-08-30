a3e2b362f14037c711e59a58551daf39fe8f9ce3
petrostrak <pit.trak@gmail.com>
2022-08-30 14:03:21 +0300

    Access and modify values with gonum mat

--- 02-Matrices-Probability-Statistics/matrices/mat.go
+++ 02-Matrices-Probability-Statistics/matrices/mat.go
@@ -19,4 +19,26 @@ func main() {
 
 	formatted := mat.Formatted(a)
 	fmt.Println(formatted)
+
+	// Get a single value from the matrix.
+	val := a.At(0, 1)
+	fmt.Printf("The value of a at (0,1) is: %.2f\n\n", val)
+
+	// Get the values on a specific column.
+	col := mat.Col(nil, 0, a)
+	fmt.Printf("The values in the 1st column are: %v\n\n", col)
+
+	// Get the values in a specific row.
+	row := mat.Row(nil, 1, a)
+	fmt.Printf("The values in the 2nd row are: %v\n\n", row)
+
+	// Modify a single element.
+	a.Set(0, 1, 11.2)
+
+	// Modify an entire row.
+	a.SetRow(0, []float64{14.3, -4.1})
+
+	// Modify an entire column.
+	a.SetCol(0, []float64{1.7, -0.3})
+	fmt.Println(formatted)
 }
